1b389f504b8b707113899a0d2dd5b69365ac578c
cleosyuu <cleo.syu@gmail.com>
2024-03-27 11:48:41 +0800

    feat: add basic dropdown component

---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  data: {
+    type: Array,
+    required: true
+  },
+  open: {
+    type: Boolean
+  }
+})
+
+const currentOpened = ref('')
+
+function toggleOpen(key) {
+  currentOpened.value = currentOpened.value === key ? '' : key
+}
+</script>
+
+<template>
+  <ul class="dropdown__wrapper">
+    <li class="dropdown__item" :class="{ open: currentOpened === item.key }" v-for="item in data" :key="item.key">
+      <button class="dropdown__btn" @click="toggleOpen(item.key)">{{ item.text }}</button>
+      <DropdownItem v-if="item.children?.length" :data="item.children" />
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.dropdown__wrapper {
+  min-height: 0;
+  overflow: hidden;
+}
+
+.dropdown__item {
+  display: grid;
+  grid-template-rows: max-content 0fr;
+  transition: grid-template-rows 300ms ease;
+}
+
+.dropdown__item.open {
+  grid-template-rows: max-content 1fr;
+}
+
+
+.dropdown__item>.dropdown__wrapper {
+  margin-left: 1em;
+}
+
+.dropdown__btn {
+  padding: 0.5em 0;
+  text-align: left;
+}
+</style>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,30 @@
+<script setup>
+import DropdownItem from './DropdownItem.vue';
+
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <div class="sidemenu">
+    <DropdownItem :data="data"></DropdownItem>
+  </div>
+</template>
+
+<style>
+.sidemenu {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  padding: 2rem;
+  min-width: 20vw;
+  color: #fff;
+  background-color: #000;
+}
+</style>
